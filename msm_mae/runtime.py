nttcslab/msm-mae

commit 11fcc4280c6e6e9a6b2d22a2599b664f8e82668a

Add return_layers=True

msm_mae/runtime.py
@@ -124,14 +124,7 @@ class RuntimeMAE(nn.Module):
         x = (x - mu) / sigma
         return x
 
-    def to_normalized_spec(self, batch_audio):
-        # raw -> spectrogram
-        x = self.to_feature(batch_audio)
-        # normalize among batch samples
-        x = self.normalize_batch(x)
-        return x
-
-    def encode_lms(self, lms):
+    def encode_lms(self, lms, return_layers=False):
         x = lms
 
         patch_fbins = self.backbone.grid_size()[0]
@@ -146,10 +139,22 @@ class RuntimeMAE(nn.Module):
         if True:
             # stack embeddings
             for i in range(x.shape[-1] // unit_frames):
-                emb, _, _ = self.backbone.forward_encoder(x[..., i*unit_frames:(i+1)*unit_frames], mask_ratio=0.)
+                emb, _, _ = self.backbone.forward_encoder(x[..., i*unit_frames:(i+1)*unit_frames], mask_ratio=0., return_layers=return_layers)
                 if self.backbone.use_cls_token:
-                    emb = emb[:, 1:, :]
-                emb = rearrange(emb, 'b (f t) d -> b t (f d)', f=patch_fbins, d=embed_d)
+                    emb = emb[..., 1:, :]
+                if len(emb.shape) > 3:
+                    emb = rearrange(emb, 'L b (f t) d -> L b t (f d)', f=patch_fbins, d=embed_d)
+                else:
+                    emb = rearrange(emb, 'b (f t) d -> b t (f d)', f=patch_fbins, d=embed_d)
+                embeddings.append(emb)
+        elif False:
+            # stack embeddings of all layers
+            for i in range(x.shape[-1] // unit_frames):
+                emb, _, _ = self.backbone.forward_encoder(x[..., i*unit_frames:(i+1)*unit_frames], mask_ratio=0., return_layers=True)
+                if self.backbone.use_cls_token:
+                    emb = emb[..., 1:, :]
+                emb = rearrange(emb, 'L b (f t) d -> L b t (f d)', f=patch_fbins, d=embed_d)
+                emb = rearrange(emb, 'L b t D -> b t (L D)')
                 embeddings.append(emb)
         elif False:
             # CLS only
@@ -166,13 +171,14 @@ class RuntimeMAE(nn.Module):
                 if self.backbone.use_cls_token:
                     emb = emb[:, 1:, :]
                 embeddings.append(emb)
-        x = torch.hstack(embeddings)
-        pad_emb_frames = int(embeddings[0].shape[1] * pad_frames / unit_frames)
+
+        x = torch.cat(embeddings, axis=-2)
+        pad_emb_frames = int(embeddings[0].shape[-2] * pad_frames / unit_frames)
         # print(2, x.shape, embeddings[0].shape, pad_emb_frames)
         if pad_emb_frames > 0:
-            x = x[:, :-pad_emb_frames] # remove padded tail
+            x = x[..., :-pad_emb_frames, :] # remove padded tail
         # print(3, x.shape)
-        return x
+        return x if len(emb.shape) == 3 else [x_ for x_ in x]
 
     def encode(self, batch_audio):
         x = self.to_normalized_spec(batch_audio)

msm_mae/patch_msm_mae.diff
@@ -6,7 +6,7 @@
  import torch.nn as nn
 +import numpy as np
 +from timm.models.vision_transformer import PatchEmbed, DropPath, Mlp
-+from util.pos_embed import get_2d_sincos_pos_embed, get_sinusoid_encoding_table
++from .pos_embed import get_2d_sincos_pos_embed, get_sinusoid_encoding_table
  
 -from timm.models.vision_transformer import PatchEmbed, Block
  
@@ -254,7 +254,13 @@
  
          # keep the first subset
          ids_keep = ids_shuffle[:, :len_keep]
-@@ -152,15 +255,19 @@
+@@ -147,26 +250,37 @@
+ 
+         return x_masked, mask, ids_restore
+ 
+-    def forward_encoder(self, x, mask_ratio):
++    def forward_encoder(self, x, mask_ratio, return_layers=False):
+         # embed patches
          x = self.patch_embed(x)
  
          # add pos embed w/o cls token
@@ -277,8 +283,21 @@
 +            x = torch.cat((cls_tokens, x), dim=1)
  
          # apply Transformer blocks
++        layers = []
          for blk in self.blocks:
-@@ -177,7 +284,10 @@
+             x = blk(x)
++            if return_layers: layers.append(x)
+         x = self.norm(x)
++        if return_layers:
++            layers.pop() # replace the last feature with the normalized one.
++            layers.append(x)
+ 
++        if return_layers:
++            return torch.stack(layers), mask, ids_restore
+         return x, mask, ids_restore
+ 
+     def forward_decoder(self, x, ids_restore):
+@@ -177,7 +291,10 @@
          mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
          x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token
          x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
@@ -290,7 +309,7 @@
  
          # add pos embed
          x = x + self.decoder_pos_embed
-@@ -191,14 +301,15 @@
+@@ -191,14 +308,15 @@
          x = self.decoder_pred(x)
  
          # remove cls token
@@ -309,7 +328,7 @@
          mask: [N, L], 0 is keep, 1 is remove, 
          """
          target = self.patchify(imgs)
-@@ -219,32 +330,92 @@
+@@ -219,32 +337,92 @@
          loss = self.forward_loss(imgs, pred, mask)
          return loss, pred, mask
  
@@ -434,3 +453,51 @@
          loss_value = loss.item()
  
          if not math.isfinite(loss_value):
+diff -u util/pos_embed.py ../util/pos_embed.py
+--- util/pos_embed.py	2022-03-31 08:30:46.053352675 +0900
++++ ../util/pos_embed.py	2022-03-14 20:49:01.497119223 +0900
+@@ -17,18 +17,19 @@
+ # Transformer: https://github.com/tensorflow/models/blob/master/official/nlp/transformer/model_utils.py
+ # MoCo v3: https://github.com/facebookresearch/moco-v3
+ # --------------------------------------------------------
+-def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
++def get_2d_sincos_pos_embed(embed_dim, grid_sizes, cls_token=False):
+     """
+     grid_size: int of the grid height and width
+     return:
+     pos_embed: [grid_size*grid_size, embed_dim] or [1+grid_size*grid_size, embed_dim] (w/ or w/o cls_token)
+     """
+-    grid_h = np.arange(grid_size, dtype=np.float32)
+-    grid_w = np.arange(grid_size, dtype=np.float32)
++    gH, gW = grid_sizes
++    grid_h = np.arange(gH, dtype=np.float32)
++    grid_w = np.arange(gW, dtype=np.float32)
+     grid = np.meshgrid(grid_w, grid_h)  # here w goes first
+     grid = np.stack(grid, axis=0)
+ 
+-    grid = grid.reshape([2, 1, grid_size, grid_size])
++    grid = grid.reshape([2, 1, gH, gW])
+     pos_embed = get_2d_sincos_pos_embed_from_grid(embed_dim, grid)
+     if cls_token:
+         pos_embed = np.concatenate([np.zeros([1, embed_dim]), pos_embed], axis=0)
+@@ -94,3 +95,20 @@
+             pos_tokens = pos_tokens.permute(0, 2, 3, 1).flatten(1, 2)
+             new_pos_embed = torch.cat((extra_tokens, pos_tokens), dim=1)
+             checkpoint_model['pos_embed'] = new_pos_embed
++
++
++# sin-cos position encoding
++# https://github.com/jadore801120/attention-is-all-you-need-pytorch/blob/master/transformer/Models.py#L31
++def get_sinusoid_encoding_table(n_position, d_hid, cls_token=False): 
++    ''' Sinusoid position encoding table ''' 
++    # TODO: make it with torch instead of numpy 
++    def get_position_angle_vec(position): 
++        return [position / np.power(10000, 2 * (hid_j // 2) / d_hid) for hid_j in range(d_hid)] 
++
++    sinusoid_table = np.array([get_position_angle_vec(pos_i) for pos_i in range(n_position)]) 
++    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2]) # dim 2i 
++    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2]) # dim 2i+1 
++
++    if cls_token:
++        sinusoid_table = np.concatenate([np.zeros([1, d_hid]), sinusoid_table], axis=0)
++    return sinusoid_table

util/patch_util.diff
@@ -1,48 +1,0 @@
-diff -u util/pos_embed.py ../util/pos_embed.py
---- util/pos_embed.py	2022-03-31 08:30:46.053352675 +0900
-+++ ../util/pos_embed.py	2022-03-14 20:49:01.497119223 +0900
-@@ -17,18 +17,19 @@
- # Transformer: https://github.com/tensorflow/models/blob/master/official/nlp/transformer/model_utils.py
- # MoCo v3: https://github.com/facebookresearch/moco-v3
- # --------------------------------------------------------
--def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
-+def get_2d_sincos_pos_embed(embed_dim, grid_sizes, cls_token=False):
-     """
-     grid_size: int of the grid height and width
-     return:
-     pos_embed: [grid_size*grid_size, embed_dim] or [1+grid_size*grid_size, embed_dim] (w/ or w/o cls_token)
-     """
--    grid_h = np.arange(grid_size, dtype=np.float32)
--    grid_w = np.arange(grid_size, dtype=np.float32)
-+    gH, gW = grid_sizes
-+    grid_h = np.arange(gH, dtype=np.float32)
-+    grid_w = np.arange(gW, dtype=np.float32)
-     grid = np.meshgrid(grid_w, grid_h)  # here w goes first
-     grid = np.stack(grid, axis=0)
- 
--    grid = grid.reshape([2, 1, grid_size, grid_size])
-+    grid = grid.reshape([2, 1, gH, gW])
-     pos_embed = get_2d_sincos_pos_embed_from_grid(embed_dim, grid)
-     if cls_token:
-         pos_embed = np.concatenate([np.zeros([1, embed_dim]), pos_embed], axis=0)
-@@ -94,3 +95,20 @@
-             pos_tokens = pos_tokens.permute(0, 2, 3, 1).flatten(1, 2)
-             new_pos_embed = torch.cat((extra_tokens, pos_tokens), dim=1)
-             checkpoint_model['pos_embed'] = new_pos_embed
-+
-+
-+# sin-cos position encoding
-+# https://github.com/jadore801120/attention-is-all-you-need-pytorch/blob/master/transformer/Models.py#L31
-+def get_sinusoid_encoding_table(n_position, d_hid, cls_token=False): 
-+    ''' Sinusoid position encoding table ''' 
-+    # TODO: make it with torch instead of numpy 
-+    def get_position_angle_vec(position): 
-+        return [position / np.power(10000, 2 * (hid_j // 2) / d_hid) for hid_j in range(d_hid)] 
-+
-+    sinusoid_table = np.array([get_position_angle_vec(pos_i) for pos_i in range(n_position)]) 
-+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2]) # dim 2i 
-+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2]) # dim 2i+1 
-+
-+    if cls_token:
-+        sinusoid_table = np.concatenate([np.zeros([1, d_hid]), sinusoid_table], axis=0)
-+    return sinusoid_table

commit 3383a213a71c3d357ffb24a7d3d5f0d07b22c27f

Fix missing to_normalized_spec caused by 11fcc4280c6e6e9a6b2d22a2599b664f8e82668a

misc/Note_test_runtime.ipynb
@@ -1,0 +1,95 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "80x208p16x16_0422\n",
+      "MaskedAutoencoderViT(patch size=(16, 16), grid_size=(5, 13),\n",
+      "  embed_dim=768, depth=12, num_heads=12, decoder_embed_dim=384,\n",
+      "  decoder_depth=4, decoder_num_heads=6, mlp_ratio=4,\n",
+      "  norm_pix_loss=False, use_cls_token=True, use_2d_dec_pos_embd=False)\n",
+      "Runtime MelSpectrogram(16000, 400, 400, 160, 80, 50, 8000):\n",
+      "MelSpectrogram(\n",
+      "  Mel filter banks size = (80, 201), trainable_mel=False\n",
+      "  (stft): STFT(n_fft=400, Fourier Kernel size=(201, 1, 400), iSTFT=False, trainable=False)\n",
+      ")\n"
+     ]
+    }
+   ],
+   "source": [
+    "import sys\n",
+    "sys.path.append('..')\n",
+    "import numpy as np\n",
+    "import torch\n",
+    "import matplotlib.pyplot as plt\n",
+    "import matplotlib.patches as mpl_patches\n",
+    "from einops import rearrange\n",
+    "from IPython.display import display, Audio\n",
+    "import textwrap\n",
+    "\n",
+    "from msm_mae.runtime import RuntimeMAE\n",
+    "\n",
+    "\n",
+    "# Create runtime instance\n",
+    "# weight = '../80x512p16x16_0425/checkpoint-100.pth'\n",
+    "weight = '../80x208p16x16_0422/checkpoint-100.pth'\n",
+    "rt = RuntimeMAE(weight_file=weight)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "True"
+      ]
+     },
+     "execution_count": 5,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# Test for the fix of the issue #1\n",
+    "# Caused by 11fcc4280c6e6e9a6b2d22a2599b664f8e82668a\n",
+    "\n",
+    "assert (rt.encode(torch.rand(8, 16000*2)).shape == torch.Size([8, 13, 3840]))"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  },
+  "vscode": {
+   "interpreter": {
+    "hash": "d47b8c551b1bcfe0fd5238b830308815317a66441517e93d4548d617a96cfeaa"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}

msm_mae/runtime.py
@@ -124,6 +124,13 @@ class RuntimeMAE(nn.Module):
         x = (x - mu) / sigma
         return x
 
+    def to_normalized_spec(self, batch_audio):
+        # raw -> spectrogram
+        x = self.to_feature(batch_audio)
+        # normalize among batch samples
+        x = self.normalize_batch(x)
+        return x
+
     def encode_lms(self, lms, return_layers=False):
         x = lms
 